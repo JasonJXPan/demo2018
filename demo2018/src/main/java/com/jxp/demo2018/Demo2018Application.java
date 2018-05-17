9593a23554b9569c7c3efa00aef620785efa6b1f
panjinxin <panjinxin@juqitech.com>
2018-05-17 21:47:47 +0800

    first java demo project

---
+++ demo2018/src/main/java/com/jxp/demo2018/Demo2018Application.java
@@ -0,0 +1,12 @@
+package com.jxp.demo2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2018Application.class, args);
+	}
+}
