ab100d46add0bce78bc1cabda09b9a4ed878ee2e
panjinxin <panjinxin@juqitech.com>
2018-05-20 17:26:10 +0800

    first java demo project

--- demo2018/src/main/java/com/pjx/demo2018/controller/DemoController.java
+++ demo2018/src/main/java/com/pjx/demo2018/controller/DemoController.java
@@ -1,7 +1,19 @@
 package com.pjx.demo2018.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * Created by juqi on 18/5/17.
+ *
+ * @author juqi
+ * @date 18/5/17
  */
+@RestController
 public class DemoController {
+
+    @RequestMapping(value = "/health_check", method = RequestMethod.GET)
+    public boolean checkProjectHealth(){
+        return true;
+    }
 }
